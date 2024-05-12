07a3a5b0789eb72351e0f9f05fffe69abf3f7fcb
Gyzoe <159135436+Gyzoe@users.noreply.github.com>
2024-05-12 16:36:18 +0200

    Update mapscript.js

--- js/mapscript.js
+++ js/mapscript.js
@@ -19,3 +19,6 @@ L.rectangle(bounds, {color: "#e60005", weight: 1}).addTo(map);
 // plaats een marker met als tekst "AP-Hogeschool" en eronder "Ellermanstraat 33"
 let apMarker = L.marker([51.23009, 4.41616]).addTo(map);
 apMarker.bindPopup("<b>AP-Hogeschool</b><br>Ellermanstraat 33").openPopup();
+
+let apMap = Location.a([51.23009, 4.41616]).addTo(map)
+
